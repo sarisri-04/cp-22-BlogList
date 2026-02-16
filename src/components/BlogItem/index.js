417e26bf3f3836be232bd2577422b1bfc41ee4cb
karunasri <mkarunashri@gmail.com>
2026-02-16 15:21:37 +0000

    Solution

---
+++ src/components/BlogItem/index.js
@@ -0,0 +1,19 @@
+// Write your JS code here
+import './index.css'
+
+const BlogItem = props => {
+  const {blogDetails} = props
+  const {title, description, publishedDate} = blogDetails
+
+  return (
+    <li className="blog-item">
+      <div className="title-date-container">
+        <h1 className="blog-title">{title}</h1>
+        <p className="blog-date">{publishedDate}</p>
+      </div>
+      <p className="blog-description">{description}</p>
+    </li>
+  )
+}
+
+export default BlogItem
